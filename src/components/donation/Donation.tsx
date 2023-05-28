6d9f52d4b74769441761eaeb5a53750708e0d05f
jasonkitfan <jasonkitfan@gmail.com>
2023-05-28 23:49:51 +0800

    update: 1. set stripe payment 2. ignore stripe file 3. stripe payment complete

--- src/components/donation/Donation.tsx
+++ src/components/donation/Donation.tsx
@@ -1,31 +1,40 @@
-import { Box, Button, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import PaymentFormWrapper from "./PaymentForm";
 
 const donateButtonText = "Donate";
-const title = "Give a big helping hand"
-const text = "Your support can make a huge difference. Donate today and help us continue our mission to help the cats. Every contribution counts and helps us make a positive impact in our community. Thank you for your generosity!";
+const title = "Give a big helping hand";
+const text =
+  "Your support can make a huge difference. Donate today and help us continue our mission to help the cats. Every contribution counts and helps us make a positive impact in our community. Thank you for your generosity!";
 
 const Donation = () => {
+  const [showPaymentForm, setShowPaymentForm] = useState(false);
+
+  const handleDonateClick = () => {
+    setShowPaymentForm(true);
+  };
+
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '16px',
-        backgroundColor: '#ffffff',
-        borderRadius: '6px',
-        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-        width: '100%',
-        maxWidth: '80%',
-        margin: '0 auto',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "16px",
+        backgroundColor: "#ffffff",
+        borderRadius: "6px",
+        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+        width: "100%",
+        maxWidth: "80%",
+        margin: "0 auto",
       }}
     >
       <Box
         sx={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          width: '100%',
-          display: { sm: 'none', md: 'flex' },
+          flexDirection: "row",
+          alignItems: "center",
+          width: "100%",
+          display: { sm: "none", md: "flex" },
         }}
       >
         <Box
@@ -33,38 +42,39 @@ const Donation = () => {
           src="https://picsum.photos/300/200"
           alt="Random"
           sx={{
-            width: '100%',
-            height: 'auto',
-            objectFit: 'cover',
-            borderRadius: '6px',
-            marginBottom: '16px',
+            width: "100%",
+            height: "auto",
+            objectFit: "cover",
+            borderRadius: "6px",
+            marginBottom: "16px",
           }}
         />
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            marginTop: '0',
-            marginBottom: '0',
-            width: '100%',
-            paddingLeft: '3%',
-            paddingRight: '3%'
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            marginTop: "0",
+            marginBottom: "0",
+            width: "100%",
+            paddingLeft: "3%",
+            paddingRight: "3%",
           }}
         >
-          <Typography variant="h2" sx={{paddingBottom: '3%'}}>{title}</Typography>
-          <Typography variant="body1" sx={{paddingBottom: '3%'}}>{text}</Typography>
-          <Button variant="contained" color="primary" sx={{ marginTop: '16px' }}>{donateButtonText}</Button>
+          {!showPaymentForm && <Typography variant="h2">{title}</Typography>}
+          {!showPaymentForm && <Typography variant="body1">{text}</Typography>}
+          {!showPaymentForm && (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleDonateClick}
+            >
+              {donateButtonText}
+            </Button>
+          )}
+          {showPaymentForm && <Typography variant="h4">Donate</Typography>}
+          {showPaymentForm && <PaymentFormWrapper />}
         </Box>
-      </Box>
-      <Box
-        sx={{
-          flexDirection: 'column',
-          alignItems: 'center',
-          width: '100%',
-          display: { sm: 'flex', md: 'none' },
-        }}
-      >
       </Box>
     </Box>
   );
---
+++ src/components/donation/PaymentForm.tsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { stripePublicKey } from "../../config/stripe";
+import { TextField, Button, Grid } from "@mui/material";
+import axios from "axios";
+
+const stripePromise = loadStripe(stripePublicKey);
+
+const PaymentForm = () => {
+  const [amount, setAmount] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handlePayment = async (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("handling payment");
+    event.preventDefault();
+
+    setLoading(true);
+
+    const cardElement = elements?.getElement(CardElement);
+
+    const { error, paymentMethod } = await stripe!.createPaymentMethod({
+      type: "card",
+      card: cardElement!,
+    });
+
+    if (error) {
+      console.log(error);
+      setLoading(false);
+      return;
+    }
+
+    if (!amount) {
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "https://asia-east2-meow-shelter.cloudfunctions.net/app/shelter/donate",
+        {
+          amount: (Number(amount) * 100).toString(),
+          paymentMethodId: paymentMethod?.id,
+        }
+      );
+      console.log("success");
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <form onSubmit={handlePayment}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            type="number"
+            label="Amount"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: "#32325d",
+                  "::placeholder": {
+                    color: "#aab7c4",
+                  },
+                },
+                invalid: {
+                  color: "#9e2146",
+                },
+              },
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <div id="card-errors" role="alert"></div>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            id="submit"
+          >
+            Submit Payment
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+const PaymentFormWrapper = () => (
+  <Elements stripe={stripePromise}>
+    <PaymentForm />
+  </Elements>
+);
+
+export default PaymentFormWrapper;
